75af91e9da1f3532953955943956e3ce77e932c2
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-08-03 12:27:20 -0700

    cargo new

---
+++ hangman/target/release/deps/hangman.d
@@ -0,0 +1,5 @@
+C:\Users\milla\Desktop\coding\Hangman\hangman\target\release\deps\hangman.exe: src\main.rs
+
+C:\Users\milla\Desktop\coding\Hangman\hangman\target\release\deps\hangman.d: src\main.rs
+
+src\main.rs:
---
+++ hangman/target/release/hangman.d
@@ -0,0 +1,1 @@
+C:\Users\milla\Desktop\coding\Hangman\hangman\target\release\hangman.exe: C:\Users\milla\Desktop\coding\Hangman\hangman\src\main.rs
